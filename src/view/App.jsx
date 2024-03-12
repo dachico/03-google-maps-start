13096c0076054e326e43d053550382e3a1fe4538
Dachi Sapir <dachisapir@gmail.com>
2024-03-12 02:59:14 +0200

    completed task + bonus tasks

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -6,34 +6,62 @@ import GoogleMap from "./GoogleMap";
 const log = (...args) => console.log.apply(null, ["App -->", ...args]);
 
 export default function App() {
-
   const [latlng, setLatlng] = useState({
     lat: -34.397,
-    lng: 150.644
+    lng: 150.644,
   });
+  const [zoom, setZoom] = useState(8);
 
-  function reposition(city) {
+  function reposition(event) {
+    const { city } = event.target.dataset;
+
     switch (city) {
       case "tel aviv":
         setLatlng({ lat: 32.0042938, lng: 34.7615399 });
+        break;
+      case "london":
+        setLatlng({ lat: 51.4567066, lng: -0.63450939 });
+        break;
+      case "paris":
+        setLatlng({ lat: 48.8589633, lng: 2.5118416 });
         break;
       default:
         alert("Location not supported");
     }
   }
 
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      setLatlng({ lat: latitude, lng: longitude });
+    });
+  };
 
-    log(latlng);
-    return (
-      <div className="app">
-        <TopBar>Google Maps Example in React</TopBar>
-        <div className="hbox mb20">
-          <button onClick={() => reposition("tel aviv")}>Tel Aviv</button>
-          <input type="number" min="8" max="16" placeholder="8" />
-        </div>
-        <GoogleMap lat={latlng.lat} lng={latlng.lng} />
-        {/* <GoogleMap {...latlng} /> */}
+  log(latlng);
+  return (
+    <div className="app">
+      <TopBar>Google Maps Example in React</TopBar>
+      <div className="hbox mb20">
+        <button onClick={() => getLocation()}>Find my location</button>
+        <button data-city="tel aviv" onClick={reposition}>
+          Tel Aviv
+        </button>
+        <button data-city="london" onClick={reposition}>
+          London
+        </button>
+        <button data-city="paris" onClick={reposition}>
+          Paris
+        </button>
+        <input
+          type="number"
+          min="8"
+          max="16"
+          placeholder="8"
+          onChange={(event) => setZoom(Number(event.target.value))}
+        />
       </div>
-    );
-  
+      <GoogleMap lat={latlng.lat} lng={latlng.lng} zoom={zoom} />
+      {/* <GoogleMap {...latlng} /> */}
+    </div>
+  );
 }
--- src/view/GoogleMap.jsx
+++ src/view/GoogleMap.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef } from "react";
 const log = (...args) => console.log.apply(null, ["GoogleMap -->", ...args]);
 
-export default function GoogleMap({ lat, lng }) {
+export default function GoogleMap({ lat, lng, zoom }) {
   const map = useRef(null);
   const mapDiv = useRef(null);
+  const infoWindow = useRef(null);
 
   async function createMap() {
     const { Map } = await google.maps.importLibrary("maps");
@@ -12,20 +13,34 @@ export default function GoogleMap({ lat, lng }) {
       center: { lat, lng },
       zoom: 8,
     });
+
+    infoWindow.current = new google.maps.InfoWindow();
+
+    map.current.addListener("click", (event) => {
+      const marker = new window.google.maps.Marker({
+        position: event.latLng,
+        map: map.current,
+      });
+
+      marker.addListener("click", (event) => {
+        infoWindow.current.setContent(`Your Location: ${event.latLng}`);
+        infoWindow.current.open(map.current, marker);
+      });
+    });
   }
   useEffect(() => {
     createMap();
   }, []);
 
   useEffect(() => {
-    if(!map.current) return;
-    log("useEffect >>>>");
-    log("lat:", lat);
-    log("lng:", lng);
-    log("mapDiv:", mapDiv);
-    log("<<<< useEffect");
+    if (!map.current) return;
     map.current.setCenter({ lat, lng });
   }, [lat, lng]);
+
+  useEffect(() => {
+    if (!map.current) return;
+    map.current.setZoom(zoom);
+  }, [zoom]);
 
   return <div ref={mapDiv} className="map-box" />;
 }
